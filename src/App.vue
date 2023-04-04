e3ec2d9118b42eb9e118a5c444bf788bff71cb77
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-03 22:40:35 -0300

    refactor: rmv -> default code generated in cli vue command

--- src/App.vue
+++ src/App.vue
@@ -7,24 +7,20 @@
 </template>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  font-family: "Public Sans", sans-serif;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  background-color: #f2f2f2;
 }
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+.main-container {
+  margin: auto;
+  width: 100%;
+  max-width: 1420px;
 }
 </style>
