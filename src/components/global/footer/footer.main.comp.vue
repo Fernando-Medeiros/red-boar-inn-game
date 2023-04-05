5693b78653be37343b688938cd06a041487ab728
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-04 21:58:04 -0300

    feat: add header and footer component

---
+++ src/components/global/footer/footer.main.comp.vue
@@ -0,0 +1,120 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "FooterMainComponent",
+  data() {
+    return {
+      routes: [
+        { path: "/atualizacoes", name: "Atualizações" },
+        { path: "/sobre", name: "Sobre" },
+      ],
+    };
+  },
+});
+</script>
+
+<template>
+  <footer class="background">
+    <div class="main-container">
+      <div class="container">
+        <div class="routes">
+          <router-link
+            v-for="route in routes"
+            :key="route.path"
+            :to="route.path"
+          >
+            {{ route.name }}
+          </router-link>
+
+          <a href="https://github.com/Fernando-Medeiros" target="_blank">
+            <p>Participe do projeto</p>
+          </a>
+        </div>
+
+        <router-link to="/">
+          <img class="image-logo" src="@/assets/logo.png" alt="logo" />
+        </router-link>
+
+        <div class="contact">
+          <div>
+            <a href="https://github.com/Fernando-Medeiros">
+              <img
+                class="image-contact"
+                src="@/../public/github.png"
+                alt="github"
+                target="_blank"
+              />
+            </a>
+          </div>
+
+          <div>
+            <a
+              href="https://www.linkedin.com/in/fernando-de-alvarenga-medeiros/"
+            >
+              <img
+                class="image-contact"
+                src="@/../public/linkedin.png"
+                alt="linkedin"
+                target="_blank"
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <p class="copy" translate="no">Red-Boar-Inn - 2023</p>
+  </footer>
+</template>
+
+<style scoped>
+.background {
+  width: 100%;
+  background-color: #222222;
+}
+.container {
+  width: 100%;
+  display: grid;
+  padding: 0.5rem 0;
+  justify-items: center;
+  justify-content: space-between;
+  grid-template-columns: 1fr 1fr 1fr;
+}
+.image-logo {
+  width: 2.5rem;
+  height: 2.5rem;
+  transition: all 1s;
+}
+.image-logo:hover {
+  transition: all 1s;
+  transform: scale(1.1);
+}
+.routes {
+  display: grid;
+  gap: 0.9rem;
+  margin: 5px;
+}
+.routes a {
+  color: #e8e8e8;
+  text-decoration: none;
+}
+.routes a:hover {
+  text-decoration: underline 2px;
+  transition: all 1s;
+}
+
+.contact {
+  display: grid;
+  gap: 0.9rem;
+}
+.image-contact {
+  width: 2rem;
+  height: 2rem;
+}
+.copy {
+  width: 100%;
+  padding: 5px 0;
+  text-align: center;
+}
+@media (max-width: 780px) {
+}
+</style>
---
+++ src/components/global/header/header.main.comp.vue
@@ -0,0 +1,106 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HeaderMainComponent",
+  data() {
+    return {
+      routes: [
+        { path: "/atualizacoes", name: "Atualizações" },
+        { path: "/sobre", name: "Sobre" },
+      ],
+    };
+  },
+});
+</script>
+
+<template>
+  <header class="background">
+    <div class="main-container">
+      <nav class="container">
+        <router-link to="/">
+          <img class="image-logo" src="@/assets/logo.png" alt="logo" />
+        </router-link>
+
+        <nav class="routes">
+          <router-link
+            v-for="route in routes"
+            :key="route.path"
+            :to="route.path"
+          >
+            {{ route.name }}
+          </router-link>
+
+          <router-link to="/">
+            <button class="btn-login"><p>Acessar</p></button>
+          </router-link>
+        </nav>
+      </nav>
+    </div>
+  </header>
+</template>
+
+<style scoped>
+.background {
+  width: 100%;
+  height: auto;
+  background-color: #222222;
+}
+.container {
+  width: 100%;
+  display: flex;
+  padding: 0.5rem 0;
+  align-items: center;
+  justify-content: space-between;
+}
+.image-logo {
+  width: 2.5rem;
+  height: 2.5rem;
+  transition: all 1s;
+  margin: 5px;
+}
+.image-logo:hover {
+  transition: all 1s;
+  transform: scale(1.1);
+}
+.routes {
+  width: auto;
+  display: flex;
+  margin: 5px;
+  align-items: center;
+}
+.routes a {
+  color: #e8e8e8;
+  text-decoration: none;
+  margin-left: 2.5rem;
+}
+.routes a:hover {
+  text-decoration: underline 2px;
+  transition: all 1s;
+}
+.btn-login {
+  height: 32px;
+  width: 75px;
+  border-radius: 4px;
+  background-color: transparent;
+  border: 1px solid #d9d9d9;
+  cursor: pointer;
+}
+.btn-login p {
+  font-size: 14px;
+  color: white;
+}
+.btn-login:hover {
+  border: 1px solid white;
+}
+
+@media (max-width: 780px) {
+  .image-logo {
+    width: 2rem;
+    height: 2rem;
+  }
+  .routes a {
+    margin-left: 1rem;
+  }
+}
+</style>
