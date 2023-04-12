740d0ffa6042bf3ad9a2f131775382944ee55b0b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-12 18:30:29 -0300

    feat: icon-static component

---
+++ src/components/global/sprite/icon-static.comp.vue
@@ -0,0 +1,36 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IconStatic",
+  props: {
+    name: { type: String, required: true },
+    label: { type: String, required: true },
+  },
+});
+</script>
+
+<template>
+  <div class="icon-static-container">
+    <img class="icon" :alt="name" :src="require(`assets/icons/${name}.png`)" />
+    <label :for="label">{{ label }}</label>
+  </div>
+</template>
+
+<style scoped>
+.icon-static-container {
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.icon {
+  width: 2rem;
+  height: 2rem;
+}
+@media (max-width: 780px) {
+  .icon {
+    width: 1rem;
+    height: 1rem;
+  }
+}
+</style>
