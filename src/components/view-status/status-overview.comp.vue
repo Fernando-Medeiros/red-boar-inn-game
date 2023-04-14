ba5cd03357acd5f58768673b4ba0cb024fff7e36
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-14 19:49:36 -0300

    feat: add Status view

---
+++ src/components/view-status/status-overview.comp.vue
@@ -0,0 +1,42 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ManagerCharacter } from "core/entities/manager-character";
+
+export default defineComponent({
+  name: "StatusOverview",
+  data() {
+    return {
+      currentStatus: {},
+    };
+  },
+  created() {
+    this.currentStatus = ManagerCharacter.status().toJson();
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div
+      class="status-overview"
+      v-for="(status, name) in currentStatus"
+      :key="status"
+    >
+      <p>{{ name }}</p>
+      <p>{{ status }}</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.status-overview {
+  display: flex;
+  padding: 10px;
+  max-width: 300px;
+  margin: auto;
+  justify-content: space-between;
+  border-bottom: 1px solid whitesmoke;
+}
+@media (max-width: 780px) {
+}
+</style>
---
+++ src/views/Status.vue
@@ -0,0 +1,84 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import { ManagerCharacter } from "core/entities/manager-character";
+
+import Sprite from "comp/global/sprite/sprite.comp.vue";
+import StatusOverview from "comp/view-status/status-overview.comp.vue";
+
+export default defineComponent({
+  name: "StatusView",
+  components: { Sprite, StatusOverview },
+
+  data() {
+    return {
+      character: {
+        getName: "",
+        getClass: "",
+        getLevel: "",
+        getGender: "",
+      },
+    };
+  },
+  created() {
+    const characterData = ManagerCharacter.character();
+
+    Object.assign(this.character, {
+      getLevel: characterData.getLevel,
+      getName: characterData.getName,
+      getClass: characterData.getClass,
+      getGender: characterData.getGender,
+    });
+  },
+});
+</script>
+
+<template>
+  <div class="background-game">
+    <div class="main-container">
+      <div class="status-container">
+        <div class="sprite-character">
+          <Sprite
+            :sprite-name="character.getClass"
+            :sprite-gender="character.getGender"
+            :rotate-y="false"
+          />
+          <span>
+            <p>{{ character.getName }}</p>
+            <span>
+              <p>
+                {{ character.getLevel }} /
+                {{ character.getClass }}
+              </p>
+            </span>
+          </span>
+        </div>
+
+        <StatusOverview />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.status-container {
+  display: grid;
+  gap: 1rem;
+}
+.sprite-character {
+  display: grid;
+  gap: 1rem;
+  justify-content: center;
+}
+.sprite-character > span {
+  display: flex;
+  gap: 1rem;
+  text-align: center;
+}
+
+@media (max-width: 780px) {
+  .sprite-character > span {
+    display: grid;
+    font-size: 14px;
+  }
+}
+</style>
