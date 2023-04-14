6b2eb43985742bb690d623728bb260f24f22163d
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-14 19:46:16 -0300

    feat: App comp -> add headerGame

--- src/App.vue
+++ src/App.vue
@@ -2,59 +2,96 @@
 import { defineComponent } from "vue";
 import { LocalStorage } from "core/middlewares/local-storage";
 import { Session } from "./core/auth/session";
+import { ManagerCharacter } from "core/entities/manager-character";
 
 import SetupHeader from "setup/global.header.json";
 import SetupFooter from "setup/global.footer.json";
 import SetupRoutes from "setup/global.routes.json";
+import SetupHeaderGame from "setup/global.header-game.json";
 
 import Header from "comp/global/header/header.main.comp.vue";
 import Footer from "comp/global/footer/footer.main.comp.vue";
-
-function getSetup() {
-  return LocalStorage.getLanguage();
-}
+import HeaderGame from "comp/global/header-game/header-game.comp.vue";
 
 export default defineComponent({
   name: "MainComponent",
-  components: { Header, Footer },
+  components: { Header, Footer, HeaderGame },
 
   data() {
     return {
+      isAuthenticated: false,
+
+      routes: [{ path: "", name: "" }],
+
       header: {
-        loginPath: "/login",
+        loginPath: "/auth/login",
         loginLabel: "",
-        isAuthenticated: false,
       },
       footer: {
         copyright: "",
         contacts: [{ path: "", name: "" }],
       },
-      routes: [{ path: "", name: "" }],
+      headerGame: {
+        inventory: { gold: "0", jewel: "0" },
+        iconsButton: [{ icon: "", label: "", route: "" }],
+      },
     };
   },
-
   created() {
-    const setupHeader = SetupHeader[getSetup()];
-    const setupFooter = SetupFooter;
-    const setupRoutes = SetupRoutes[getSetup()];
+    this.isAuthenticated = Session.isAuthenticated();
 
-    this.routes = setupRoutes.routes;
+    this.isAuthenticated
+      ? [this.loadInventory(), this.loadHeaderGame()]
+      : [this.loadHeader()];
 
-    this.header.loginLabel = setupHeader.loginLabel;
-    this.header.isAuthenticated = Session.isAuthenticated();
+    this.loadFooter();
+    this.loadRoutes();
+  },
+  methods: {
+    loadRoutes() {
+      const { routes } = SetupRoutes[LocalStorage.getLanguage()];
+      this.routes = routes;
+    },
 
-    this.footer.copyright = setupFooter.info.copyright;
-    this.footer.contacts = setupFooter.contacts;
+    loadHeader() {
+      const { loginLabel } = SetupHeader[LocalStorage.getLanguage()];
+      this.header.loginLabel = loginLabel;
+    },
+    loadFooter() {
+      const { contacts, info } = SetupFooter;
+      this.footer.copyright = info.copyright;
+      this.footer.contacts = contacts;
+    },
+
+    loadHeaderGame() {
+      const { icons } = SetupHeaderGame[LocalStorage.getLanguage()];
+      this.headerGame.iconsButton = icons;
+    },
+    loadInventory() {
+      const character = ManagerCharacter;
+
+      Object.assign(this.headerGame.inventory, {
+        gold: String(character.inventory().findMaterial("gold")?.total),
+        jewel: String(character.inventory().findMaterial("jewel")?.total),
+      });
+    },
   },
 });
 </script>
 
 <template>
   <Header
-    v-show="header.isAuthenticated === false"
+    v-if="isAuthenticated === false"
     :routes="routes"
     :login-path="header.loginPath"
     :button-label="header.loginLabel"
+  />
+
+  <HeaderGame
+    v-if="isAuthenticated === true"
+    :gold="headerGame.inventory.gold"
+    :jewel="headerGame.inventory.jewel"
+    :icons-button="headerGame.iconsButton"
   />
 
   <div class="view-container">
@@ -74,9 +111,11 @@ export default defineComponent({
   padding: 0;
   font-family: "Public Sans", sans-serif;
 }
+
 html {
   background-color: #222222;
 }
+
 #app {
   color: #e8e8e8;
   background-image: linear-gradient(#2222225b, #2222225b, #222222c0),
@@ -86,12 +125,21 @@ html {
   background-position: top;
   background-attachment: fixed;
 }
+
 .main-container {
   max-width: 1000px;
   margin: auto;
   width: 100%;
 }
+
 .view-container {
   margin-bottom: 20px;
+}
+
+.background-game {
+  margin-top: 1rem;
+  padding: 1rem 10px;
+  border-radius: 5px;
+  background: linear-gradient(#282828e1, #323232a1);
 }
 </style>
