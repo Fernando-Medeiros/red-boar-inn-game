3d2d3d548c18e4464ab77ad5d9f1cc49bba51c3b
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-04-25 22:48:45 -0300

    feat(view): Options -> guide -> accountOptions

--- src/views/Options.vue
+++ src/views/Options.vue
@@ -1,18 +1,20 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import AlertMessage from "comp/global/helpers/alert-message.comp.vue";
-import OptionsGuides from "comp/game/options/guides.comp.vue";
-import OptionsCharacter from "comp/game/options/character-options.comp.vue";
-import OptionsSystem from "comp/game/options/system-options.comp.vue";
+import Guides from "comp/game/options/guides.comp.vue";
+import CharacterOptions from "comp/game/options/character-options.comp.vue";
+import SystemOptions from "comp/game/options/system-options.comp.vue";
+import AccountOptions from "comp/game/options/account-options.comp.vue";
 
 export default defineComponent({
   name: "OptionsView",
   expose: ["alertMessage"],
   components: {
     AlertMessage,
-    OptionsGuides,
-    OptionsCharacter,
-    OptionsSystem,
+    Guides,
+    CharacterOptions,
+    SystemOptions,
+    AccountOptions,
   },
   data() {
     return {
@@ -24,6 +26,9 @@ export default defineComponent({
     showGuide(guide: string) {
       this.activeGuide = guide;
     },
+    receiveMessage(message: string) {
+      this.alertMessage = message;
+    },
   },
 });
 </script>
@@ -33,14 +38,19 @@ export default defineComponent({
 
   <div class="background-game">
     <div class="main-container">
-      <OptionsGuides @show-guide="showGuide" :current-guide="activeGuide" />
+      <Guides @show-guide="showGuide" :current-guide="activeGuide" />
 
-      <OptionsCharacter
-        v-if="activeGuide === 'character'"
-        @emit-message="(message: string) => (alertMessage = message)"
+      <CharacterOptions
+        v-show="activeGuide === 'character'"
+        @emit-message="receiveMessage"
       />
 
-      <OptionsSystem v-if="activeGuide === 'system'" />
+      <AccountOptions
+        v-show="activeGuide === 'account'"
+        @emit-message="receiveMessage"
+      />
+
+      <SystemOptions v-show="activeGuide === 'system'" />
     </div>
   </div>
 </template>
