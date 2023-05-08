d5ab4b6d5761134cb5bf29e85d79574c1ca7b7c7
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-05-07 21:05:34 -0300

    feat(comp) -> ItemSprite

---
+++ src/components/global/sprites/ItemSprite.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+defineProps<{ name: string }>();
+</script>
+
+<template>
+  <span>
+    <img
+      class="item-sprite"
+      :alt="name"
+      :src="require(`assets/items/${name}.png`)"
+    />
+  </span>
+</template>
+
+<style scoped>
+.item-sprite {
+  width: 2rem;
+  height: 2rem;
+}
+</style>
