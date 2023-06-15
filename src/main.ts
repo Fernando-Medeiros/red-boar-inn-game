e615ec1fefa979f5ad632875e7a3353197e0ca61
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-14 21:51:24 -0300

    build(plugin): vite-plugin-vuetify & vuetify

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
@@ -5,5 +7,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-
+app.use(createVuetify())
 app.mount('#app')
