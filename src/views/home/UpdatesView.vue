68aa6a0dc8740d6ec5068f656cff2be32bd5aa15
Fernando-Medeiros <fernandobasilides@gmail.com>
2023-06-14 22:00:13 -0300

    feat(view): UpdatesView -> expansion-panels

--- src/views/home/UpdatesView.vue
+++ src/views/home/UpdatesView.vue
@@ -3,36 +3,46 @@ import Helpers from 'core/helpers/Helpers'
 import SetupUpdates from 'setup/page.updates.json'
 import BannerTitle from 'comp/global/banners/BannerTitle.vue'
 import BannerSprites from 'comp/global/banners/BannerSprites.vue'
-import UpdatePreview from 'comp/home/UpdatePreview.vue'
 
 const { title } = SetupUpdates[Helpers.translate()]
 const { updates } = SetupUpdates[Helpers.translate()]
 </script>
 
 <template>
-    <div>
-        <BannerTitle :title="title" />
+    <BannerTitle :title="title" />
+    <BannerSprites :sprite-left="'warrior'" :sprite-right="'warrior'" />
 
-        <BannerSprites :sprite-left="'warrior'" :sprite-right="'warrior'" />
+    <div class="main-background">
+        <div class="main-container">
+            <v-expansion-panels :disabled="false" multiple>
+                <v-expansion-panel v-for="update in updates.reverse()" :key="update.version">
+                    <v-expansion-panel-title class="version">
+                        <p>v-{{ update.version.toFixed(1) }}</p>
+                        <p>{{ update.date }}</p>
+                    </v-expansion-panel-title>
 
-        <div class="main-background">
-            <div class="main-container">
-                <div class="updates-container">
-                    <UpdatePreview :updates="updates.reverse()" />
-                </div>
-            </div>
+                    <v-expansion-panel-text>
+                        <ul style="list-style: none">
+                            <li
+                                v-for="updateDescription in update.description"
+                                :key="updateDescription"
+                            >
+                                {{ updateDescription }}
+                            </li>
+                        </ul>
+                    </v-expansion-panel-text>
+                </v-expansion-panel>
+            </v-expansion-panels>
         </div>
     </div>
 </template>
 
 <style scoped>
-.updates-container {
-    z-index: 1;
+.version {
     display: grid;
-    gap: 20px;
-    margin-top: 1rem;
-    padding: 20px 10px;
-    justify-items: center;
-    background-color: var(--cor-background-color);
+    gap: 1rem;
+    text-align: left;
+    font-size: 16px;
+    grid-template-columns: auto 1fr;
 }
 </style>
